b4b212cbcee02ae91e13f6d1fc045c47f6da9f70
Jose Osses / Analista Programador <jooswebs@gmail.com>
2017-04-21 20:49:50 -0300

    caja diarias, gastos etc.

--- lubricentro.sql
+++ lubricentro.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 08-04-2017 a las 09:49:07
+-- Tiempo de generación: 21-04-2017 a las 20:47:12
 -- Versión del servidor: 5.5.42
 -- Versión de PHP: 5.6.10
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `lubricentro`
 --
-CREATE DATABASE IF NOT EXISTS `lubricentro` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci;
-USE `lubricentro`;
 
 -- --------------------------------------------------------
 
@@ -28,12 +26,21 @@ USE `lubricentro`;
 -- Estructura de tabla para la tabla `caja_diaria`
 --
 
-CREATE TABLE IF NOT EXISTS `caja_diaria` (
+CREATE TABLE `caja_diaria` (
   `id` int(11) NOT NULL,
   `caja_inicial` int(11) NOT NULL,
   `fecha_caja` date NOT NULL,
   `estado` varchar(15) COLLATE utf8_spanish2_ci NOT NULL DEFAULT 'ABIERTA'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `caja_diaria`
+--
+
+INSERT INTO `caja_diaria` (`id`, `caja_inicial`, `fecha_caja`, `estado`) VALUES
+(2, 50000, '2017-04-09', 'ABIERTA'),
+(5, 70000, '2017-04-10', 'ABIERTA'),
+(6, 50000, '2017-04-16', 'ABIERTA');
 
 -- --------------------------------------------------------
 
@@ -41,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `caja_diaria` (
 -- Estructura de tabla para la tabla `carrito`
 --
 
-CREATE TABLE IF NOT EXISTS `carrito` (
+CREATE TABLE `carrito` (
   `id` int(11) NOT NULL,
   `codigo_producto` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
   `nombre_producto` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
@@ -55,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `carrito` (
 -- Estructura de tabla para la tabla `categoria`
 --
 
-CREATE TABLE IF NOT EXISTS `categoria` (
+CREATE TABLE `categoria` (
   `id_categoria` int(11) NOT NULL,
   `nombre_categoria` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
@@ -79,20 +86,19 @@ INSERT INTO `categoria` (`id_categoria`, `nombre_categoria`) VALUES
 -- Estructura de tabla para la tabla `gastos`
 --
 
-CREATE TABLE IF NOT EXISTS `gastos` (
+CREATE TABLE `gastos` (
   `id` int(11) NOT NULL,
   `importe` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `comentario` text COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `gastos`
 --
 
 INSERT INTO `gastos` (`id`, `importe`, `fecha`, `comentario`) VALUES
-(6, 122, '2017-04-08', 'asdasd'),
-(7, 123, '2017-04-08', '123');
+(8, 2500, '2017-04-10', 'completo');
 
 -- --------------------------------------------------------
 
@@ -100,12 +106,12 @@ INSERT INTO `gastos` (`id`, `importe`, `fecha`, `comentario`) VALUES
 -- Estructura de tabla para la tabla `log_ventas`
 --
 
-CREATE TABLE IF NOT EXISTS `log_ventas` (
+CREATE TABLE `log_ventas` (
   `id` int(11) NOT NULL,
   `codigo_producto` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
   `cantidad` int(11) NOT NULL,
   `fecha_venta` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `log_ventas`
@@ -124,7 +130,14 @@ INSERT INTO `log_ventas` (`id`, `codigo_producto`, `cantidad`, `fecha_venta`) VA
 (12, '073579515145', 1, '2017-03-30 16:47:06'),
 (13, '7806358001675', 1, '2017-04-07 23:05:44'),
 (14, '7806358000180', 1, '2017-04-07 23:05:44'),
-(15, '7804627180748', 1, '2017-04-08 09:04:39');
+(15, '7804627180748', 1, '2017-04-08 09:04:39'),
+(16, '4045989607013', 1, '2017-04-10 01:35:13'),
+(17, '6944259838981', 1, '2017-04-10 10:27:24'),
+(18, '008536590101', 1, '2017-04-10 10:27:24'),
+(19, '7806358000180', 1, '2017-04-10 10:27:24'),
+(20, '7806358001675', 1, '2017-04-10 10:27:24'),
+(21, '079340375314', 1, '2017-04-10 10:59:31'),
+(22, '093371000670', 1, '2017-04-10 10:59:31');
 
 -- --------------------------------------------------------
 
@@ -132,7 +145,7 @@ INSERT INTO `log_ventas` (`id`, `codigo_producto`, `cantidad`, `fecha_venta`) VA
 -- Estructura de tabla para la tabla `productos`
 --
 
-CREATE TABLE IF NOT EXISTS `productos` (
+CREATE TABLE `productos` (
   `id` int(11) NOT NULL,
   `id_categoria` int(11) NOT NULL,
   `nombre_producto` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
@@ -151,12 +164,12 @@ INSERT INTO `productos` (`id`, `id_categoria`, `nombre_producto`, `precio`, `fec
 (7, 1, 'Elf 10w40  Evolution', 23000, '2017-01-04', '7804627180663', 0),
 (8, 1, 'Elf 20w50 Evolution', 19000, '2017-01-04', '7804627180748', 3),
 (9, 1, 'Acdelco 15w40 Select', 15000, '2017-01-04', '021625918471', 0),
-(10, 1, 'Mobil 10w40 Special', 14000, '2017-01-04', '7806358001675', 1),
-(11, 1, 'Mobil 20w50 Super 1000', 21500, '2017-01-04', '7806358000180', 2),
+(10, 1, 'Mobil 10w40 Special', 14000, '2017-01-04', '7806358001675', 0),
+(11, 1, 'Mobil 20w50 Super 1000', 21500, '2017-01-04', '7806358000180', 1),
 (12, 1, 'Castrol 80w90 Axle', 5500, '2017-01-04', '079191226155', 0),
-(14, 3, 'Liquido freno 1/4 ml', 2000, '2017-01-04', '008536590101', 83),
+(14, 3, 'Liquido freno 1/4 ml', 2000, '2017-01-04', '008536590101', 82),
 (15, 3, 'Alto al humo JohnsenÂ´s', 4500, '2017-01-04', '039101046266', 8),
-(16, 3, 'Limpia Injector Diesel', 3000, '2017-01-04', '6944259838981', 3),
+(16, 3, 'Limpia Injector Diesel', 3000, '2017-01-04', '6944259838981', 2),
 (17, 3, 'Limpia Injector Bencinero', 2500, '2017-01-04', '6944259838974', 5),
 (18, 3, 'Anticorrosivo radiador', 3700, '2017-01-04', '073579515145', 1),
 (19, 3, 'Limpia Injector y Carburador', 3000, '2017-01-04', '7702155009640', 4),
@@ -164,17 +177,17 @@ INSERT INTO `productos` (`id`, `id_categoria`, `nombre_producto`, `precio`, `fec
 (21, 3, 'Gear Oil (Corona)', 3500, '2017-01-04', '076906031196', 2),
 (22, 3, 'Grasa Liquida', 4500, '2017-01-04', '039101046044', 5),
 (23, 3, 'Limpia contacto johnsens', 5500, '2017-01-04', '039101046006', 6),
-(24, 3, 'Rost Off Wurth', 4100, '2017-01-04', '4045989607013', 2),
+(24, 3, 'Rost Off Wurth', 4100, '2017-01-04', '4045989607013', 1),
 (25, 3, 'Super lub Loctite', 3500, '2017-01-04', '7891200343300', 18),
 (26, 5, 'Fijador de roscas', 3750, '2017-01-04', '686226271006', 0),
 (27, 5, 'Silicona  roja Loctite', 3000, '2017-01-04', '079340374690', 13),
 (28, 5, 'Silicona Grey Loctite', 4755, '2017-01-04', '079340374645', 17),
 (29, 5, 'Silicona Gasket Versachem', 2500, '2017-01-04', '078727730098', 11),
-(30, 5, 'acero liquido', 4750, '2017-01-04', '079340375314', 10),
+(30, 5, 'acero liquido', 4750, '2017-01-04', '079340375314', 9),
 (31, 3, 'Lubricante de Chapa', 3550, '2017-01-04', '077678002506', 3),
 (32, 3, 'Limpia Radiador Versachem', 1800, '2017-01-04', '7805315000195', 4),
 (33, 3, 'Sella radiador Versachem', 1800, '2017-01-04', '7805315000171', 2),
-(34, 5, 'Adhesivo espejo CPI', 2000, '2017-01-04', '093371000670', 10),
+(34, 5, 'Adhesivo espejo CPI', 2000, '2017-01-04', '093371000670', 9),
 (35, 5, 'Reparacion Estanque Versachem', 2850, '2017-01-04', '078727167092', 4),
 (36, 6, 'Grasa Homocinetica Optimus', 3500, '2017-01-05', '7501799109156', 16),
 (37, 6, 'Grasa Rodamientos Azul Valvoline', 1500, '2017-01-05', '7808720800015', 24),
@@ -239,13 +252,20 @@ INSERT INTO `productos` (`id`, `id_categoria`, `nombre_producto`, `precio`, `fec
 -- Estructura de tabla para la tabla `ticketventa`
 --
 
-CREATE TABLE IF NOT EXISTS `ticketventa` (
+CREATE TABLE `ticketventa` (
   `id_ticket` int(11) NOT NULL,
   `rut_usuario` varchar(14) COLLATE utf8_spanish2_ci NOT NULL,
   `monto` int(11) NOT NULL,
   `fecha_ticket` datetime NOT NULL,
   `comentario` text COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `ticketventa`
+--
+
+INSERT INTO `ticketventa` (`id_ticket`, `rut_usuario`, `monto`, `fecha_ticket`, `comentario`) VALUES
+(1, '17.757.169-5', 50000, '2017-04-10 01:36:50', 'sdasdasdasd');
 
 -- --------------------------------------------------------
 
@@ -253,7 +273,7 @@ CREATE TABLE IF NOT EXISTS `ticketventa` (
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
+CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `rut` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
   `password` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
@@ -332,7 +352,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `caja_diaria`
 --
 ALTER TABLE `caja_diaria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `carrito`
 --
@@ -347,12 +367,12 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de la tabla `gastos`
 --
 ALTER TABLE `gastos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT de la tabla `log_ventas`
 --
 ALTER TABLE `log_ventas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
@@ -362,7 +382,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `ticketventa`
 --
 ALTER TABLE `ticketventa`
-  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
